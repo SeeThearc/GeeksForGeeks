afc2268654c5d831fd4a20ec1f105e533f2c7df3
SeeThearc <ayushagrawal2334@gmail.com>
2025-06-01 23:44:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    // Function to count inversions in the array.
+    int merge(vector<int>&arr,int low,int mid,int high){
+        vector<int>temp;
+        int left=low;
+        int right=mid+1;
+        int cnt=0;
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                cnt+=mid-left+1;
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.push_back(arr[left++]);
+        }
+        while(right<=high){
+            temp.push_back(arr[right++]);
+        }
+        for(int i=low;i<=high;i++){
+            arr[i] = temp[i-low];
+        }
+        return cnt;
+    }
+    int mergesort(vector<int>&arr,int low,int high){
+        int cnt=0;
+        if(low>=high){
+            return cnt;
+        }
+        int mid=(low+high)/2;
+        cnt+=mergesort(arr,low,mid);
+        cnt+=mergesort(arr,mid+1,high);
+        cnt+=merge(arr,low,mid,high);
+        return cnt;
+    }
+    int inversionCount(vector<int> &arr) {
+        // Your Code Here
+        int ans=mergesort(arr,0,arr.size()-1);
+        return ans;
+    }
+};
