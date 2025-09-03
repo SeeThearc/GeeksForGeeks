2308bc4c84e5bfb11642a662908964001e06b5b9
SeeThearc <ayushagrawal2334@gmail.com>
2025-09-03 18:28:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,32 +1,35 @@
 class Solution {
   public:
-    void dfs(vector<vector<int>>&adjLis,int node,stack<int>&st,vector<int>&vis){
-        vis[node]=1;
-        for(auto it:adjLis[node]){
-            if(!vis[it]){
-                dfs(adjLis,it,st,vis);
-            }
-        }
-        st.push(node);
-    }
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
+        queue<int>q;
         vector<vector<int>>adjLis(V);
+        vector<int>inDegree(V,0);
         for(int i=0;i<edges.size();i++){
             adjLis[edges[i][0]].push_back(edges[i][1]);
+            inDegree[edges[i][1]]++;
         }
         vector<int>vis(V,0);
-        stack<int>st;
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfs(adjLis,i,st,vis);
+            if(inDegree[i]==0){
+                q.push(i);
+                vis[i]=1;
             }
         }
         vector<int>ans;
-        while(!st.empty()){
-            int temp = st.top();
-            st.pop();
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
             ans.push_back(temp);
+            for(auto it:adjLis[temp]){
+                inDegree[it]--;
+            }
+            for(int i=0;i<V;i++){
+                if(!vis[i] && inDegree[i]==0){
+                    q.push(i);
+                    vis[i]=1;
+                }
+            }
         }
         return ans;
     }
